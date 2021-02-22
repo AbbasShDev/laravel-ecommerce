993db60cef1e7e6f1f595be5b37e36510fbe95e4
abbasShDev <abbas20alzaeem@gmail.com>
2021-02-22 06:35:59 +0300

    Product Image Gallery done

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -24,12 +24,17 @@
 
         <div>
             <div class="product-section-image">
-                <img src="{{presentImage($product->image) }}" alt="product">
+                <img src="{{presentImage($product->image) }}" class="active" alt="product" id="currentImage">
             </div>
-            <div>
+            <div class="product-section-images">
+                <div class="product-section-thumbnails selected">
+                    <img src="{{presentImage($product->image) }}" alt="product">
+                </div>
                 @if($product->images)
                     @foreach(json_decode($product->images, true) as $image)
-                        <img src="{{presentImage($image) }}" alt="product">
+                        <div class="product-section-thumbnails">
+                            <img src="{{presentImage($image) }}" alt="product">
+                        </div>
                     @endforeach
                 @endif
             </div>
@@ -40,7 +45,7 @@
             <div class="product-section-price">{{ $product->presentPrice() }}</div>
             <p>{{ $product->getTranslatedAttribute('details') }}</p>
 
-{{--            <a href="#" class="button">Add to Cart</a>--}}
+            {{--            <a href="#" class="button">Add to Cart</a>--}}
             <form action="{{ route('cart.store', $product->slug) }}" method="post">
                 @csrf
                 <button type="submit" class="button button-plain">Add to Cart</button>
@@ -50,5 +55,27 @@
 
     @include('partials.might-like')
 
+@endsection
 
+@section('extra-js')
+    <script>
+        (function () {
+            const cureentImage = document.querySelector('#currentImage');
+            const images = document.querySelectorAll('.product-section-thumbnails');
+
+            images.forEach((el) => el.addEventListener('click', thumbnailClick))
+
+            function thumbnailClick(e) {
+                cureentImage.classList.remove('active');
+
+                cureentImage.addEventListener('transitionend', () => {
+                    cureentImage.src = this.querySelector('img').src;
+                    cureentImage.classList.add('active');
+                })
+
+                images.forEach((el) => el.classList.remove('selected'))
+                this.classList.add('selected')
+            }
+        }())
+    </script>
 @endsection
