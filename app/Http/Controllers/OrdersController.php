64ffcecb318adcb1c391017091090d67c6040eb3
abbasShDev <abbas20alzaeem@gmail.com>
2021-03-02 20:59:45 +0300

    Prevent visting from not their owner

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -12,13 +12,16 @@ class OrdersController extends Controller
     {
         $orders = auth()->user()->orders()->with('products')->latest()->get();
 
-
         return view('orders.index', compact('orders'));
     }
 
 
     public function show(Order $order)
     {
+        if (auth()->id() !== $order->user_id){
+            return abort('401');
+        }
+
         $products = $order->products;
         return view('orders.show', compact('order', 'products'));
     }
